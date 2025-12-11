eadf9c0a2205955a30f7064c9c3a212c1e29d568
Noel Zemicael <noel.zemicael@yh.nackademin.se>
2025-12-11 22:25:50 +0100

    final commit.

--- index.js
+++ index.js
@@ -1,14 +1,47 @@
-// valideringskontroll
-
 const form = document.querySelector('.contact-form');
 
+// Error fields
+const nameError = document.getElementById('name-error');
+const emailError = document.getElementById('email-error');
+const messageError = document.getElementById('message-error');
+
+// Validation runs when someone submits the forms.
 form.addEventListener('submit', function(e) {
+    let hasError = false;
+
+    // Clear old errors
+    nameError.textContent = "";
+    emailError.textContent = "";
+    messageError.textContent = "";
+
+    // Removes spaces in name, email and message to control better.
     const name = form.name.value.trim();
     const email = form.email.value.trim();
     const message = form.message.value.trim();
 
-    if (!name || !email || !message) {
+    // Name empty
+    if (!name) {
+        nameError.textContent = "Name is required.";
+        hasError = true;
+    }
+
+    // Email empty or invalid missing @ or a dot.
+    if (!email) {
+        emailError.textContent = "Email is required.";
+        hasError = true;
+    } else if (!email.includes("@") || !email.includes(".")) {
+        emailError.textContent = "Email must be valid.";
+        hasError = true;
+    }
+
+    // Message too short
+    if (message.length < 10) {
+        messageError.textContent = "Message must be at least 10 characters.";
+        hasError = true;
+    }
+
+    // Wont submit if there's a error
+    if (hasError) {
         e.preventDefault();
-        alert('Please fill out all fields.');
     }
 });
